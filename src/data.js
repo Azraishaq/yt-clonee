3f699db92caa96cb07b0c52217247adbd5a0026e
Azraishaq <152273710+Azraishaq@users.noreply.github.com>
2024-06-13 14:54:51 +0530

    intial

---
+++ src/data.js
@@ -0,0 +1,22 @@
+ export const API_KEY='AIzaSyADP-cuGRKsvRGcS3YRkgrs_gJh-UDTrg0';
+
+
+
+  export const value_converter=(value)=>{
+if(value>=1000000){
+
+    return  Math.floor(value/1000000)+"M";
+
+}
+else if (value>=1000){
+return Math.floor(value/1000)+"K"
+
+
+}
+else {
+
+
+    return value;
+}
+
+ }
